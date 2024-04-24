da3fe28d4b20df6480494397f31c4bde297504fb
hoangtrong <hoangcongtrong054@gmail.com>
2024-04-24 18:26:49 +0700

    Update gitignore

--- server/django/smart_trask/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('model/', include('api.model.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
