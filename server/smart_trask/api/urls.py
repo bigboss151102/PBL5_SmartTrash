5cbb85dfd748315abac691cb8dcabe590bfd52ee
hoangtrong <hoangcongtrong054@gmail.com>
2024-05-02 10:29:19 +0700

    Updated code uppp

--- server/smart_trask/api/urls.py
+++ server/smart_trask/api/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from .views import *
+
+account_register_add_api = AccountRegisterMVS.as_view({
+    'post': 'account_register_add_api'
+})
+
+account_login_api = AccountLoginMVS.as_view({
+    'post': 'account_login_api'
+})
+
+account_reset_password_api = AccountResetPasswordMVS.as_view({
+    'post': 'account_reset_password_api'
+})
 
 
 urlpatterns = [
-    path('account/', include('api.account.urls')),
+    path('account_register_add_api/', account_register_add_api),
+    path('account_login_api/', account_login_api),
+    path('account_reset_password_api/', account_reset_password_api),
     path('model/', include('api.model.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
