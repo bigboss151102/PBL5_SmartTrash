a48c8ef0e047684ff71815f8ba124aaf4c9126ac
hoangtrong <hoangcongtrong054@gmail.com>
2024-05-07 12:02:54 +0700

    Push code get average garbage_compartment by each name_country

--- server/smart_trask/api/garbage/views.py
+++ server/smart_trask/api/garbage/views.py
@@ -8,6 +8,7 @@ import requests
 from django.conf import settings
 import os
 from django.db.models import Count
+from django.db.models import Avg, F, Subquery, OuterRef
 
 from .serializers import *
 # from api.pagination import *
@@ -123,7 +124,95 @@ class GarbageCompartmentMVS(viewsets.ModelViewSet):
             # print(serializer.data)
             return Response(data=serializer.data, status=status.HTTP_200_OK)
         except Exception as error:
-            print("GarbageMVS_get_distance_is_full_compartment_by_id_api: ", error)
+            print(
+                "GarbageCompartmentMVS_get_distance_is_full_compartment_by_id_api: ", error)
+        return Response({'error': 'Bad request'}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(methods=['GET'], detail=False, url_path="get_average_distance_is_full_compartment_by_all_garbage_api", url_name="get_average_distance_is_full_compartment_by_all_garbage_api")
+    def get_average_distance_is_full_compartment_by_all_garbage_api(self, request, *args, **kwargs):
+        try:
+            #     # Subquery để lấy các giá trị duy nhất của name_country từ Garbage
+            #     data = {
+            #         "name_country": None,
+            #         "value_average": {
+            #             "Metal": 0,
+            #             "Plastic": 0,
+            #             "Paper": 0,
+            #             "Another": 0
+            #         }
+            #     }
+            #     subquery = Garbage.objects.filter(id=OuterRef(
+            #         'garbage_id')).values('name_country')[:1]
+            #     # print("Các giá trị name_country: ", subquery)
+            #     # Câu truy vấn chính để tính giá trị trung bình của distance_is_full cho mỗi loại ngăn (Metal, Plastic, Paper)
+            #     average_distances = GarbageCompartment.objects.filter(
+            #         garbage__name_country=Subquery(subquery)
+            #     ).values('garbage__name_country', 'type_name_compartment').annotate(avg_distance=Avg('distance_is_full'))
+
+            #     # Tạo một dictionary để lưu trữ giá trị trung bình của distance_is_full cho mỗi loại ngăn của mỗi khu vực
+            #     average_distance_dict = {}
+
+            #     # Lặp qua kết quả của truy vấn và gán giá trị trung bình vào từng loại ngăn của mỗi khu vực
+            #     for compartment in average_distances:
+            #         name_country = compartment['garbage__name_country']
+            #         compartment_type = compartment['type_name_compartment']
+            #         avg_distance = compartment['avg_distance']
+
+            #         if name_country not in average_distance_dict:
+            #             average_distance_dict[name_country] = {}
+
+            #         average_distance_dict[name_country][compartment_type] = avg_distance
+
+            #     # In ra kết quả
+            #     for name_country, compartment_data in average_distance_dict.items():
+            #         print(f"Name Country: {name_country}")
+            #         for compartment, avg_distance in compartment_data.items():
+            #             print(
+            #                 f"- Average distance_is_full for {compartment}: {avg_distance}")
+
+           # Khởi tạo danh sách để lưu trữ thông tin của mỗi name_country
+            data_list = []
+
+            # Subquery để lấy các giá trị duy nhất của name_country từ Garbage
+            subquery = Garbage.objects.filter(id=OuterRef(
+                'garbage_id')).values('name_country')
+
+            # Câu truy vấn chính để tính giá trị trung bình của distance_is_full cho mỗi loại ngăn (Metal, Plastic, Paper)
+            average_distances = GarbageCompartment.objects.filter(
+                garbage__name_country__in=subquery
+            ).values('garbage__name_country', 'type_name_compartment').annotate(avg_distance=Avg('distance_is_full'))
+
+            # Lặp qua kết quả của truy vấn và gán giá trị trung bình vào danh sách data_list
+            for compartment in average_distances:
+                name_country = compartment['garbage__name_country']
+                compartment_type = compartment['type_name_compartment']
+                avg_distance = compartment['avg_distance']
+
+                # Tìm hoặc tạo mới một entry cho name_country trong data_list
+                found = False
+                for data in data_list:
+                    if data["name_country"] == name_country:
+                        found = True
+                        data["value_average"][compartment_type] = avg_distance
+                        break
+
+                # Nếu name_country chưa tồn tại trong data_list, tạo mới một entry cho nó
+                if not found:
+                    data_list.append({
+                        "name_country": name_country,
+                        "value_average": {
+                            "Metal": 0,
+                            "Plastic": 0,
+                            "Paper": 0,
+                            "Another": 0
+                        }
+                    })
+                    data_list[-1]["value_average"][compartment_type] = avg_distance
+            # print(data_list)
+            return Response(data=data_list, status=status.HTTP_200_OK)
+        except Exception as error:
+            print(
+                "GarbageCompartmentMVS_get_distance_is_full_compartment_by_id_api: ", error)
         return Response({'error': 'Bad request'}, status=status.HTTP_400_BAD_REQUEST)
 
 
